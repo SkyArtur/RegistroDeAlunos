866368c25342f17be160ba70df2daa6f13ec552b
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-04-02 03:39:54 -0300

    fix: correção de bugs de imports apontados Actions

--- core/objects/__init__.py
+++ core/objects/__init__.py
@@ -1,3 +1,5 @@
-from .aluno import Pessoa, Boletim, Aluno
+from .pessoa import Pessoa
+from .boletim import Boletim
+from .aluno import Aluno
 
 __all__ = ['Pessoa', 'Boletim', 'Aluno']
--- core/objects/pessoa.py
+++ core/objects/pessoa.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Optional
 from abc import ABC, abstractmethod
 from dataclasses import dataclass,field
