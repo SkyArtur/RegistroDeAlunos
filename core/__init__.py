e6f50a546ddcfc1377cb21d85c2d975e948afce6
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-04-02 01:36:05 -0300

    "feat: desenvolvimento do objeto pessoa"

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,14 @@
 from .logger import create_logger
 from .validators import validar_numeros, validar_datas
+from .objects import Pessoa
 
 
-__all__ = ['create_logger', 'validar_numeros', 'validar_datas']
+__all__ = [
+    # logger
+    'create_logger',
+    # validators
+    'validar_numeros',
+    'validar_datas',
+    # objects
+    'Pessoa'
+]
