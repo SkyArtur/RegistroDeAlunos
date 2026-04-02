efe30f563b7f513968bcf05b0a7a57567212667c
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-04-01 23:14:03 -0300

    "chore: configuração dos imports de `core`"

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+from .logger import create_logger
+from .validators import validar_numeros, validar_datas
+
+
+__all__ = ['create_logger', 'validar_numeros', 'validar_datas']
---
+++ core/validators/__init__.py
@@ -0,0 +1,5 @@
+from .validar_datas import validar_datas
+from .validar_numeros import validar_numeros
+
+
+__all__ = ['validar_numeros', 'validar_datas']
