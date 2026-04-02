1f4d4027fdb1b2490609e9ec3978aeb2659b65dd
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-04-02 20:53:57 -0300

    feat: adicionado a função `caminho_database`

--- core/functions/filer.py
+++ core/functions/filer.py
@@ -6,3 +6,11 @@ def criar_arquivo(file: Path) -> Path:
     if not file.exists():
         file.touch()
     return file
+
+def caminho_database(*, debug: bool = False) -> Path:
+    raiz = Path(__file__).parent.parent.parent.resolve()
+    diretorio_database = raiz.joinpath('database')
+    diretorio_testes = raiz.joinpath('tests')
+    if debug:
+        return criar_arquivo(diretorio_testes / 'fixtures/artifacts/db_test.sqlite').resolve()
+    return criar_arquivo(diretorio_database / 'data/db.sqlite').resolve()
