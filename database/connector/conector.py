0f36f709d601dc8c51caf7f13a063ddf1b2bc8ba
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-04-02 19:47:58 -0300

    feat: criação da classe Conector

--- database/connector/conector.py
+++ database/connector/conector.py
@@ -3,15 +3,9 @@ from pathlib import Path
 
 
 class Conector:
-    __instance = None
     __slots__ = ['conexao']
 
-    def __new__(cls, *args, **kwargs):
-        if cls.__instance is None:
-            cls.__instance = super(Conector, cls).__new__(cls)
-        return cls.__instance
-
-    def __init__(self, *, arquivo: Path | str, **kwargs) -> None:
+    def __init__(self, *, arquivo: Path | str) -> None:
         self.conexao = sqlite3.connect(arquivo)
 
     def cursor(self) -> sqlite3.Cursor:
