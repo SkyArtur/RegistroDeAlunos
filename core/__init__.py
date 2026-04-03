fe79c3bfa06261966fca0aec4c45fab06938242a
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-04-03 14:23:55 -0300

    refactor: ajuste dos imports

--- core/__init__.py
+++ core/__init__.py
@@ -2,6 +2,7 @@ from .logger import create_logger
 from .functions import criar_arquivo, caminho_database
 from .validators import validar_numeros, validar_datas
 from .objects import Pessoa, Boletim, Aluno
+from .inputs import entrada_padrao, entrada_numeros, entrada_datas
 
 __all__ = [
     # functions
@@ -15,5 +16,9 @@ __all__ = [
     # objects
     'Pessoa',
     'Boletim',
-    'Aluno'
+    'Aluno',
+    # inputs
+    'entrada_padrao',
+    'entrada_numeros',
+    'entrada_datas'
 ]
