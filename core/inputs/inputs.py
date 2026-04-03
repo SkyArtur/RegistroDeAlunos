f83a6179758d1b4a0696fe99d1a82001e37b1309
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-04-03 14:24:18 -0300

    feat: funções para controle de entrada de dados

---
+++ core/inputs/inputs.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+from .decorator import entrada_decorator
+from core import validar_numeros, validar_datas
+
+@entrada_decorator
+def entrada_numeros(entrada: str, **kwargs) -> float | int:
+    return validar_numeros(entrada, **kwargs)
+
+@entrada_decorator
+def entrada_datas(entrada: str) -> date:
+    return validar_datas(entrada)
+
+@entrada_decorator
+def entrada_padrao(entrada: str) -> str:
+    return entrada
