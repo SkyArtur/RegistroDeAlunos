d69e7ae3802734bb1393a322977c5ee741daf5ca
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-04-03 14:23:14 -0300

    feat: função decoradora para controle de dados inválido `entrada_decorator`

---
+++ core/inputs/decorator.py
@@ -0,0 +1,26 @@
+from functools import wraps
+from typing import Callable
+
+from core.validators.exceptions import ValidadoresError
+from core import create_logger
+from .exceptions import EntradaTentativasError
+
+
+logger = create_logger()
+
+
+def entrada_decorator(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(entrada, *, config_tentativas: int = 3, **kwargs):
+        tentativas = config_tentativas
+        while tentativas > 0:
+            try:
+                _input = func(input(entrada), **kwargs)
+                return _input
+            except (ValidadoresError, ) as e:
+                mensagem = getattr(e, "message", str(e))
+                detalhe = getattr(e, "detalhe", func.__name__)
+                logger.warning(mensagem, extra={'detalhe': detalhe})
+                tentativas -= 1
+        raise EntradaTentativasError(func.__name__)
+    return wrapper
